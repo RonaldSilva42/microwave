664bada07f86dda722bb849654032e14f90dc163
Beatriz de Araújo Andrade <80129016+BeatrizA2@users.noreply.github.com>
2022-05-06 23:00:30 -0300

    Update freq_div.v

--- Encoder/freq_div.v
+++ Encoder/freq_div.v
@@ -1,19 +1,20 @@
 module freq_div(input wire clock,
                 output wire div_clk);
   
-  interger count = 0;
+  integer count = 0;
   
   always @(posedge clock)
     begin
-      if(count == 100)
+      if(count < 100)
         begin
-          div_clk = 1;
-          count = 0;
+          div_clk = 0;
         end
       else
         begin
-          div_clk = 0;
-          count = count + 1;
+          count = 0;
+          div_clk = 1;
         end
+      
+      count = count + 1;
     end
 endmodule
