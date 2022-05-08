eb54417d873a8300d4e233ce057fc1978cdac031
Beatriz de Araújo Andrade <80129016+BeatrizA2@users.noreply.github.com>
2022-05-08 13:46:09 -0300

    Update decoder.v

--- Decoder/decoder.v
+++ Decoder/decoder.v
@@ -5,7 +5,7 @@ module decoder(input wire[3:0] sec_ones, sec_tens, min,
     				(min == 4'b0001) ? 7'b011_0000 : 
     				(min == 4'b0010) ? 7'b110_1101 : 
     				(min == 4'b0011) ? 7'b111_1001 : 
-            (min == 4'b0100) ? 7'b011_0011 : 
+                    (min == 4'b0100) ? 7'b011_0011 : 
     				(min == 4'b0101) ? 7'b101_1011 : 
     				(min == 4'b0110) ? 7'b001_1111 : 
     				(min == 4'b0111) ? 7'b111_0000 :
@@ -14,8 +14,8 @@ module decoder(input wire[3:0] sec_ones, sec_tens, min,
     				7'b000_0000;
   
  
-  assign sec_tens_segs =  ((min == 4'b0000 ) && (sec_tens == 4'b0000)) ? 7'b000_0000:
-    					            (sec_tens == 4'b0000) ? 7'b111_1110 :
+  assign sec_tens_segs =  ((min_segs == 7'b000_0000) && (sec_tens == 4'b0000)) ? 7'b000_0000:
+    					  (sec_tens == 4'b0000) ? 7'b111_1110 :
                           (sec_tens == 4'b0001) ? 7'b011_0000 :
                           (sec_tens == 4'b0010) ? 7'b110_1101 :
                           (sec_tens == 4'b0011) ? 7'b111_1001 :
@@ -27,8 +27,8 @@ module decoder(input wire[3:0] sec_ones, sec_tens, min,
                           (sec_tens == 4'b1001) ? 7'b111_0011 : 
                           7'b000_0000;
  
-  assign sec_ones_segs = ((min == 4'b0000 ) && (sec_tens == 4'b0000) && (sec_ones == 4'b0000)) ? 7'b000_0000:
-     					           (sec_ones == 4'b0000) ? 7'b111_1110 :
+  assign sec_ones_segs = ((min_segs == 7'b000_0000) && (sec_tens_segs == 7'b000_0000) && (sec_ones == 4'b0000)) ? 7'b000_0000:
+     					 (sec_ones == 4'b0000) ? 7'b111_1110 :
                          (sec_ones == 4'b0001) ? 7'b011_0000 :
                          (sec_ones == 4'b0010) ? 7'b110_1101 :
                          (sec_ones == 4'b0011) ? 7'b111_1001 :
@@ -39,6 +39,5 @@ module decoder(input wire[3:0] sec_ones, sec_tens, min,
                          (sec_ones == 4'b1000) ? 7'b111_1111 :
                          (sec_ones == 4'b1001) ? 7'b111_0011 :
                          7'b000_0000;
-  
   
 endmodule
