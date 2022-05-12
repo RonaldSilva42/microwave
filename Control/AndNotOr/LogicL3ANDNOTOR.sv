e9ab5dc164c906dbf83569c00b15398679c26c91
Beatriz de Araújo Andrade <80129016+BeatrizA2@users.noreply.github.com>
2022-05-12 19:19:37 -0300

    Delete Control directory

--- Control/AndNotOr/LogicL3ANDNOTOR.sv
+++
@@ -1,6 +0,0 @@
-module logic_and_or_not(input start, input stop, input clear, input door_closed, input timer_done, output wire set, output wire reset);
-	
-  	assign set = (start && door_closed);
-  	assign reset = (stop || clear || ~door_closed || timer_done); 
-  
-endmodule
