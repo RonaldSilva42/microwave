5d2d0fa2fa04e45c71fc4cea879b7cebd6dffd12
José Ronald <jronaldsilva11@gmail.com>
2022-05-12 19:31:23 -0300

    acho q agora o encoder pega

--- Encoder/Encoder/encoder.v
+++ Encoder/Encoder/encoder.v
@@ -3,19 +3,19 @@ module encoder(
     input clk, enablen,
 
     output wire [3:0] D,
-    output wire pgt_1hz, loadn
+    output wire pgt_1hz, loadn, enable
 );
-    assign enablen = ~enablen;
+    assign enable = ~enablen;
 
     wire valid, div_clk, pgt;
 
-    priority_encoder p_encoder (.keypad(keypad), .enablen(enablen), .D(D), .valid(valid));
+    priority_encoder p_encoder (.keypad(keypad), .enablen(enable), .D(D), .valid(valid));
 
     freq_div div (.clock(clk), .div_clk(div_clk));
 
     counter_mod7 counter (.clk(clk), .clear(valid), .pgt(pgt));
 
-    mux mux1 (.sel(enablen), .pgt(pgt), .clk(div_clk), .out(pgt_1hz));
+    mux mux1 (.sel(enable), .pgt(pgt), .clk(div_clk), .out(pgt_1hz));
 
     assign loadn = ~valid;
 
