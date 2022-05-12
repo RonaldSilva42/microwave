0fc1f3250b4db49b9e601acd795fe907a84fe6a0
Beatriz de Araújo Andrade <80129016+BeatrizA2@users.noreply.github.com>
2022-05-12 18:53:00 -0300

    Update mod10.v

--- Timer/Mod10/mod10.v
+++ Timer/Mod10/mod10.v
@@ -10,37 +10,29 @@ module mod_10_bcd(
   //zero = 1 quando ones = 4'b000
 
   assign tc = ((ones == 4'b0000) && (en == 1)) ? 1 : 0;
-  assign zero = (ones == 4'b0000) ? 1 : 0;
+  assign zero = (ones === 4'b0000 || ones === 4'bXXXX) ? 1 : 0;
   
-  always@(posedge clk, negedge clrn)
-    begin
-      
-      if(!clrn)
-        begin
-          ones <= 0;
-        end
-      else
-        begin
+  always@(negedge clrn) begin
+    ones <= 0;
+  end
+  
+  always@(posedge clk) begin
           if(!en)
             begin
               if(!loadn)
-                begin
                   ones <= data;
-                end
             end
           else
             begin
               if(ones == 0)
-                begin
                   ones <= 9;
-                end
               else
-                begin
-                	ones <= ones - 1;
-                end
+                ones <= ones - 1;
             end
         end
-    end
   
   
 endmodule
+  
+  
+    
